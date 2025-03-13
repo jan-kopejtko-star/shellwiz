78d1376ee6a8a287bbd494a09af6f0bfc7aa0615
Jan Kopejtko <jan.kopejtko@appspark.dev>
2025-03-13 12:14:12 +0100

    .

--- app.py
+++ app.py
@@ -6,10 +6,47 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """Home page route (Quiz Game)"""
     return render_template('index.html')
+
+@app.route('/premium')
+def premium():
+    """Premium page route"""
+    return render_template('premium.html')
+
+@app.route('/tutorial')
+def tutorial():
+    """Tutorial page route"""
+    return render_template('tutorial.html')
+
+@app.route('/quiz')
+def quiz():
+    """Quiz page route - currently same as index"""
+    return render_template('index.html')
+
+@app.route('/challenges')
+def challenges():
+    """Challenges page route"""
+    return render_template('challenges.html')
+
+@app.route('/leaderboard')
+def leaderboard():
+    """Leaderboard page route"""
+    return render_template('leaderboard.html')
+
+@app.route('/commands')
+def commands():
+    """Commands reference page route"""
+    return render_template('commands.html')
+
+@app.route('/about')
+def about():
+    """About page route"""
+    return render_template('about.html')
 
 @app.route('/static/<path:path>')
 def send_static(path):
+    """Route for serving static files"""
     return send_from_directory('static', path)
 
 if __name__ == '__main__':
